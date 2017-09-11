8f0e7e70ceea80cbd5fb05adbf7bb115e9755b44
JonnyDem <tl-k@ukr.net>
2017-09-11 10:49:26 +0300

    11.09.17

--- src/classwork/lesson5/checkurl/NoUrlExeption.java
+++ src/classwork/lesson5/checkurl/NoUrlExeption.java
@@ -5,7 +5,7 @@ import java.io.PrintStream;
 public class NoUrlExeption extends Exception {
 
     @Override
-    public void printStackTrace(PrintStream s) {
+    public void printStackTrace() {
         System.err.println("No url");
     }
 }
---
+++ src/classwork/lesson7/testanotation/Small.java
@@ -0,0 +1,9 @@
+package classwork.lesson7.testanotation;
+
+public class Small extends Big {
+
+    @Override
+    public void big() {
+        super.big();
+    }
+}
